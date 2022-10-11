c2a0dc23873cb1a19be1bcb07661d135c337a0c2
Akulox <maksimmartenyuk@gmail.com>
2022-10-11 21:39:41 +0300

    Init

---
+++ Floating Cubes/Assets/Script/Cube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    public class Cube : MonoBehaviour
+    {
+        public float speed;
+        public float distance;
+        
+        private Rigidbody _rigidbody;
+        private Vector3 startPosition;
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            startPosition = transform.position;
+            _rigidbody.velocity = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * Vector3.right * speed;
+        }
+        private void FixedUpdate()
+        {
+            if (Vector3.Distance(startPosition, transform.position) >= distance)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Floating Cubes/Assets/Script/CubeSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script
+{
+    public class CubeSpawner : MonoBehaviour
+    {
+        public InputField speedInput;
+        public InputField distanceInput;
+        public GameObject cubePrefab;
+        public float spawnCD;
+        private float nextSpawn;
+
+        void FixedUpdate()
+        {
+            if (Time.time > nextSpawn)
+            {
+                nextSpawn = Time.time + spawnCD;
+                Cube cube = Instantiate(cubePrefab, transform.position, Quaternion.Euler(0,Random.Range(0, 360),0)).GetComponent<Cube>();
+
+                if (!float.TryParse(speedInput.text, out var speed) || speed < 0.5f)
+                {
+                    speed = 1f;
+                }
+                if (!float.TryParse(distanceInput.text, out var distance) || distance < 0.5f)
+                {
+                    distance = 1f;
+                }
+                
+                cube.speed = speed;
+                cube.distance = distance;
+            }
+        }
+    }
+}
